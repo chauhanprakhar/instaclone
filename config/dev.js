aa389495a639296aa999f406d84dcecb64208fd6
prakhar <201951116@iiitvadodara.ac.in>
2020-07-05 09:23:28 +0530

    good

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://prakhar_chauhan:sft2MFCPDJ1MLjHq@cluster0.4uci2.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET : "jncuwebcuinucud"
+}
